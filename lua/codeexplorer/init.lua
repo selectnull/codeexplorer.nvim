cf767c27fd0485443611e7252df92fddd8a1543f
Sasha Matijasic <sasha@selectnull.com>
2025-01-15 01:47:39 +0100

    Store symbols in module's field

--- lua/codeexplorer/init.lua
+++ lua/codeexplorer/init.lua
@@ -1,32 +1,36 @@
+local M = {}
+
 local lsp = require "codeexplorer.lsp"
 
---- Close the CodeExplorer window and move the cursor to selected symbol
+---@class codeexplorer.Position
+---@field row integer
+---@field col integer
+
+---@class codeexplorer.Symbol
+---@field name string
+---@field kind string
+---@field position codeexplorer.Position
+
+--- Close the CodeExplorer window and/or move the cursor to selected symbol
 local function set_current_line()
   local current_line = vim.api.nvim_win_get_cursor(0)[1]
-  local line_content = vim.api.nvim_buf_get_lines(0, current_line - 1, current_line, false)[1]
-
-  local row, col = string.match(line_content, "- (%d+):(%d+)$")
 
   -- close the CodeExplorer window
   vim.api.nvim_win_close(0, false)
 
   -- set the cursor position
-  if row and col then
-    vim.api.nvim_win_set_cursor(0, { tonumber(row), tonumber(col) - 1 })
+  -- header height is 2 lines, skip it
+  if current_line > 2 then
+    local selected = M._symbols[current_line - 2]
+    vim.api.nvim_win_set_cursor(0, { selected.position.row, selected.position.col - 1 })
   end
 end
 
---- Create a CodeExplorer window
----@param output [string] The text content (a list of lines) of the window
-local function create_window(output)
-  local buf = vim.api.nvim_create_buf(false, true)
-  local width = 60
-  local header_height = 2
-  local height = #output + header_height
-
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "CodeExplorer", string.rep("—", width) })
-  vim.api.nvim_buf_set_lines(buf, header_height + 1, -1, false, output)
-
+--- Create a floating window
+---@param buf integer Buffer number
+---@param width integer Window width
+---@param height integer Window height
+local function create_window(buf, width, height)
   local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
     width = width,
@@ -37,6 +41,27 @@ local function create_window(output)
     border = "rounded",
   })
   vim.api.nvim_win_set_cursor(win, { 3, 0 })
+
+  return win
+end
+
+--- Render symbols
+---@param symbols codeexplorer.Symbol[] The list of symbols to render
+local function render_symbols(symbols)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = 60
+  local header_height = 2
+  local height = #symbols + header_height
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "CodeExplorer", string.rep("—", width) })
+  local lines = {}
+  for _, symbol in ipairs(symbols) do
+    local line = symbol.name .. " (" .. symbol.kind .. ")"
+    table.insert(lines, line)
+  end
+  vim.api.nvim_buf_set_lines(buf, header_height + 1, -1, false, lines)
+
+  create_window(buf, width, height)
 
   -- Set buffer options
   local opts = { buf = 0 }
@@ -50,8 +75,8 @@ local function create_window(output)
 end
 
 --- Query the Language Server for the document symbols
----@param callback function
-local function query_symbols(callback)
+---@param render_callback function
+local function query_symbols(render_callback)
   local request_handler = function(err, result, _, _)
     local symbols = {}
     if err ~= nil then
@@ -59,18 +84,18 @@ local function query_symbols(callback)
       return
     end
 
+    local new_symbol = { name = nil, kind = nil, position = { -1, -1 } }
+
     for _, symbol in ipairs(result) do
-      local line = "("
-        .. lsp.get_kind_name(symbol.kind)
-        .. ") "
-        .. symbol.name
-        .. " - "
-        .. symbol.selectionRange.start.line + 1
-        .. ":"
-        .. symbol.selectionRange.start.character + 1
-      table.insert(symbols, line)
+      new_symbol = {
+        name = symbol.name,
+        kind = lsp.get_kind_name(symbol.kind),
+        position = { row = symbol.selectionRange.start.line + 1, col = symbol.selectionRange.start.character + 1 },
+      }
+      table.insert(symbols, new_symbol)
     end
-    callback(symbols)
+    M._symbols = symbols
+    render_callback(symbols)
   end
 
   vim.lsp.buf_request(
@@ -83,5 +108,7 @@ end
 
 --- CodeExplorer command
 vim.api.nvim_create_user_command("CodeExplorer", function()
-  query_symbols(create_window)
+  query_symbols(render_symbols)
 end, {})
+
+return M
