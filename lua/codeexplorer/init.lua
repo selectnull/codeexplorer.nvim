f111f13258b5540db3e33e775be560bbd6fdf0e8
Sasha Matijasic <sasha@selectnull.com>
2025-01-25 20:07:09 +0100

    Move definitions to separate file

--- lua/codeexplorer/init.lua
+++ lua/codeexplorer/init.lua
@@ -3,15 +3,6 @@ local M = {}
 local lsp = require "codeexplorer.lsp"
 local ui = require "codeexplorer.ui"
 
----@class codeexplorer.Position
----@field row integer
----@field col integer
-
----@class codeexplorer.Symbol
----@field name string
----@field kind string
----@field position codeexplorer.Position
-
 --- CodeExplorer user command
 vim.api.nvim_create_user_command("CodeExplorer", function()
   local filename = vim.api.nvim_buf_get_name(0)
---
+++ lua/defs.lua
@@ -0,0 +1,10 @@
+--- definitions
+
+---@class codeexplorer.Position
+---@field row integer
+---@field col integer
+
+---@class codeexplorer.Symbol
+---@field name string
+---@field kind string
+---@field position codeexplorer.Position
